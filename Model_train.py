f137093a5ed8e83d30d60b94eb2ded2486564595
Kumari Rashmi <rashmi05pathak@gmail.com>
2022-02-24 19:17:44 +0530

    code clean - training network using optimisers

--- Model_train.py
+++ Model_train.py
@@ -59,57 +59,12 @@ def Model_train():            #define inputs
   output = softmax 
   activation = activ
   
-  #Forward propogation
-  def forward_propogation(weights, bias, x):
-    Acti = {}
-    H = {}
-    
-    H['H0'] = x
-    
-    for i in range(1,num_hidden_layers+1):
-      Acti[('A%s'% i)] = np.matmul(weights['W%s'% i],H[('H%s'% (i-1))])+bias['b%s'% i]
-      H['H%s'% i] = activation(Acti['A%s'% i])
-    
-    Acti['A%s'%(num_hidden_layers+1)]= np.matmul(weights['W%s'% (num_hidden_layers+1)],H['H%s'% (num_hidden_layers)])+bias['b%s'% (num_hidden_layers+1)]
-    y_hat = output(Acti['A%s'% (num_hidden_layers+1)])  
-    
-    return y_hat, Acti, H
-  
-
-  #Backward propogation
-  def backward_propogation(w, b, x, y):
-    grad_A = {}
-    grad_W = {}
-    grad_b = {}
-    grad_H = {}  
-
-    y_hat, A, H = forward_propogation(w, b, x)
-
-    grad_A['A%s'% (num_hidden_layers+1)] = -(y-y_hat)  #cross entropy loss
-
-    for i in range(num_hidden_layers+1, 1, -1):
-      
-      grad_W['W%s'%i] = np.matmul(grad_A['A%s'%i].reshape(-1,1), H['H%s'%(i-1)].reshape(1,-1))
-
-      grad_b['b%s'%i] = grad_A['A%s'%i]
-
-      grad_H['H%s'%(i-1)] = np.dot(np.transpose(w['W%s'%i]), grad_A['A%s'%i]) 
-
-      grad_A['A%s'%(i-1)] = np.multiply(grad_H['H%s'%(i-1)], der_activ(A['A%s'%(i-1)]))
-
-    grad_W['W1'] = np.multiply(grad_A['A1'].reshape(-1,1), H['H0'].reshape(1,-1))
-
-    grad_b['b1'] = grad_A['A1']
-
-    return grad_W, grad_b
-    
-
    #defining function for sgd optimizer
   def sgd():
 
     for k in range(epochs):
       for x,y in zip(X,Y):
-        grad_w,grad_b=backward_propogation(weights, bias, x,y)
+        grad_w,grad_b = backward_propogation(weights, bias, x,y)
         for j in range(1,num_hidden_layers+2):
           
           weights['W%s'%j]-=(grad_w['W%s'%j])*eta
@@ -119,19 +74,19 @@ def Model_train():            #define inputs
   def momentum_gd():
 
     gamma=0.9
-    prev_dw,prev_db=init_zeros()
+    prev_dw,prev_db=init_zeros(num_weight_mat,num_nodes)
 
     for k in range(epochs):
       count=0   
     
-      dw,db=init_zeros()
+      dw,db=init_zeros(num_weight_mat,num_nodes)
       
       for x,y in zip(X,Y):
         
         if count==0:
-          dw,db=init_zeros()
+          dw,db=init_zeros(num_weight_mat,num_nodes)
         
-        grad_w,grad_b=backward_propogation(weights, bias, x, y)
+        grad_w,grad_b = backward_propogation(weights, bias, x, y)
 
         for j in range(1,num_hidden_layers+2):
           dw['W%s'%j]+=(grad_w['W%s'%j])
@@ -155,10 +110,10 @@ def Model_train():            #define inputs
   def nesterov():
 
     gamma=0.9
-    prev_vw,prev_vb=init_zeros()
-    temp_w,temp_b=init_zeros()
-    dw,db=init_zeros()
-    v_w,v_b=init_zeros()
+    prev_vw,prev_vb=init_zeros(num_weight_mat,num_nodes)
+    temp_w,temp_b=init_zeros(num_weight_mat,num_nodes)
+    dw,db=init_zeros(num_weight_mat,num_nodes)
+    v_w,v_b=init_zeros(num_weight_mat,num_nodes)
 
     for k in range(epochs):
       count=0
@@ -171,7 +126,7 @@ def Model_train():            #define inputs
 
       for x,y in zip(X,Y):
         if count==0:
-          dw,db=init_zeros()
+          dw,db=init_zeros(num_weight_mat,num_nodes)
 
         for j in range(1,num_hidden_layers+2):
           temp_w['W%s'%j]=weights['W%s'%j]-v_w['W%s'%j]
@@ -200,39 +155,6 @@ def Model_train():            #define inputs
   
     eps = 1e-8
 
-    v_w,v_b=init_zeros()
-
-    for k in range(epochs):
-      count=0
-      dw,db=init_zeros()
-      
-      for x,y in zip(X,Y):
-
-        if count==0:
-          dw,db=init_zeros()
-        
-        grad_w,grad_b=backward_propogation( weights,bias,x,y)
-        for j in range(1,num_hidden_layers+2):
-          dw['W%s'%j]+=(grad_w['W%s'%j])
-          db['b%s'%j]+=(grad_b['b%s'%j])
-        count+=1
-
-        if count%batch_size==0:
-
-          for j in range(1,num_hidden_layers+2):
-            v_w['W%s'%j] += np.square(dw['W%s'%j])
-            v_b['b%s'%j] += np.square(db['b%s'%j]) 
-            weights['W%s'%j] -= ((dw['W%s'%j])*(eta/np.sqrt(v_w['W%s'%j] + eps))+(weights['W%s'%j])*wt_decay*eta)
-            bias['b%s'%j] -= ((db['b%s'%j])*(eta/np.sqrt(v_b['b%s'%j] + eps))+(bias['b%s'%j])*wt_decay*eta)
-
-            count=0
-
-   #defining function for rmsprop optimizer
-  def rmsprop():
-  
-    eps = 1e-8
-    beta1 = 0.9
-
     v_w,v_b=init_zeros(num_weight_mat,num_nodes)
 
     for k in range(epochs):
@@ -253,6 +175,37 @@ def Model_train():            #define inputs
         if count%batch_size==0:
 
           for j in range(1,num_hidden_layers+2):
+            v_w['W%s'%j] += np.square(dw['W%s'%j])
+            v_b['b%s'%j] += np.square(db['b%s'%j]) 
+            weights['W%s'%j] -= ((dw['W%s'%j])*(eta/np.sqrt(v_w['W%s'%j] + eps))+(weights['W%s'%j])*wt_decay*eta)
+            bias['b%s'%j] -= ((db['b%s'%j])*(eta/np.sqrt(v_b['b%s'%j] + eps))+(bias['b%s'%j])*wt_decay*eta)
+
+            count=0
+  #defining function for rmsprop optimizer
+  def rmsprop():
+    eps = 1e-8
+    beta1 = 0.9
+
+    v_w,v_b=init_zeros(num_weight_mat,num_nodes)
+
+    for k in range(epochs):
+      count=0
+      dw,db=init_zeros(num_weight_mat,num_nodes)
+      
+      for x,y in zip(X,Y):
+
+        if count==0:
+          dw,db=init_zeros(num_weight_mat,num_nodes)
+        
+        grad_w,grad_b= backward_propogation( weights,bias,x,y)
+        for j in range(1,num_hidden_layers+2):
+          dw['W%s'%j]+=(grad_w['W%s'%j])
+          db['b%s'%j]+=(grad_b['b%s'%j])
+        count+=1
+
+        if count%batch_size==0:
+
+          for j in range(1,num_hidden_layers+2):
             v_w['W%s'%j] = beta1*v_w['W%s'%j] + (1-beta1)*np.square(dw['W%s'%j])
             v_b['b%s'%j] = beta1*v_b['b%s'%j] + (1-beta1)*np.square(db['b%s'%j]) 
             weights['W%s'%j] -= ((dw['W%s'%j])*(eta/np.sqrt(v_w['W%s'%j] + eps))+(weights['W%s'%j])*wt_decay*eta)
@@ -260,6 +213,7 @@ def Model_train():            #define inputs
 
             count=0
 
+  
    #defining function for adam optimizer
   def adam():
   
@@ -267,21 +221,21 @@ def Model_train():            #define inputs
     beta1 = 0.9
     beta2 = 0.999
 
-    v_w,v_b=init_zeros()
-    v_w_hat,v_b_hat=init_zeros()
+    v_w,v_b=init_zeros(num_weight_mat,num_nodes)
+    v_w_hat,v_b_hat=init_zeros(num_weight_mat,num_nodes)
 
-    m_w,m_b=init_zeros()
-    m_w_hat,m_b_hat=init_zeros()
+    m_w,m_b=init_zeros(num_weight_mat,num_nodes)
+    m_w_hat,m_b_hat=init_zeros(num_weight_mat,num_nodes)
 
     for k in range(epochs):
       
       count=0
-      dw,db=init_zeros()
+      dw,db=init_zeros(num_weight_mat,num_nodes)
       
       for x,y in zip(X,Y):
 
         if count==0:
-          dw,db=init_zeros()
+          dw,db=init_zeros(num_weight_mat,num_nodes)
         
         grad_w,grad_b = backward_propogation( weights,bias,x,y)
         for j in range(1,num_hidden_layers+2):
@@ -327,26 +281,26 @@ def Model_train():            #define inputs
     print('Optimization algo given is not available')
   #function for errors and validation accuracy 
 
-  def error(y, y_hat):
-      return -np.log(np.dot(y, y_hat))
+  def cross_entropy(p, q):
+      return -np.log(np.dot(p, q))
 
   def val_accuracy(weights,bias,x_val,y_val):
      y_pred=np.zeros(10000)
      fx=np.zeros([10000,10])
      val_loss=0
      for i in range(10000):
-        fx[i]= Train().forward_propogation(weights,bias,x_val[i])[0]
-        val_loss+=error(y_val[i],fx[i])
+        fx[i]= forward_propogation(weights,bias,x_val[i])[0]
+        val_loss+= cross_entropy(y_val[i],fx[i])
      val_loss/=10000
      y_pred=np.argmax(fx,axis=1)   
      y_val_=np.argmax(y_val,axis=1)
 
-     val_accuracy=accuracy_score(y_pred,y_val_)
-     return val_loss,val_accuracy
+     validation_acc = accuracy_score(y_pred,y_val_)
+     return val_loss,validation_acc
 
-  val_loss,val_acc = val_accuracy(weights,bias,x_val,y_val)
+  val_loss,val_accuracy = val_accuracy(weights,bias,x_val,y_val)
   print(val_loss) #to know how the model is performing on validation set
 
-  params={'epochs':epochs,'learning rate':eta,'no of hidden layers':num_hidden_layers,'layer size':size,'batch size':batch_size,'optimizer':optimization,'val loss':val_loss,'validation accuracy':val_acc}
+  params={'epochs':epochs,'learning rate':eta,'no of hidden layers':num_hidden_layers,'layer size':size,'batch size':batch_size,'optimizer':optimization,'val loss':val_loss,'validation accuracy':val_accuracy}
 
   return weights,bias
