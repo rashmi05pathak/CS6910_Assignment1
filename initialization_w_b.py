8228ea0150bfdb7f2b85ed0a447a3549360f2bea
Kumari Rashmi <rashmi05pathak@gmail.com>
2022-02-24 19:19:23 +0530

    Adding activation function here - code clean

--- initialization_w_b.py
+++ initialization_w_b.py
@@ -1,3 +1,29 @@
+#Defining activation functions and their derivatives
+
+def sigmoid(x):
+  return 1/(1+np.exp(-x))
+
+def tanh(x):
+  return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+
+def relu(x):
+  t=np.array((np.maximum(0,x)),dtype=np.longdouble)
+  return t
+
+def grad_tanh(x):
+  return 1-(tanh(x))**2
+
+def grad_relu(x):
+  t = np.where(x < 0, 0, x)
+  return t
+
+def grad_sigmoid(x):
+  return (sigmoid(x)*(1-sigmoid(x)))
+
+def softmax(x):
+  exps = np.exp(x )
+  return exps / np.sum(exps)
+
 #Defining initialization of weight and bias: random and xavier initialization
 def init_random(num_weight_mat,num_nodes):
     weights={}
@@ -18,6 +44,7 @@ def init_xavier(num_weight_mat,num_nodes):
     return weights, bias
 
 def init_zeros(num_weight_mat,num_nodes):
+    #print(num_weight_mat)
     weights={}
     bias = {}
     for i in  range(1,num_weight_mat+1):
