89977b6109b017a3dcf6aef460bb4cc7a03fceb6
Kunal <94509717+KunalPatil8020@users.noreply.github.com>
2022-02-24 22:26:54 +0530

    Wandb

--- Image Display.py
+++ Image Display.py
@@ -1,4 +1,6 @@
 # getting one sample image from each class and visualizing 
+import wandb
+
 (trainx, trainy), (testx, testy)= fashion_mnist.load_data()
 classes = np.unique(trainy)
 index_mat = [0]*10
@@ -12,3 +14,7 @@ for j, i in enumerate(index_mat):
   plt.imshow(trainx[i], cmap=plt.get_cmap('gray'))
   
 plt.show()
+
+class_names=['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+wandb.init(entity='kunal_patil',project='Assignment 1')
+wandb.log({'sample':[wandb.Image(trainx[i],caption=class_names[trainy[i]]) for i in index_mat]})
